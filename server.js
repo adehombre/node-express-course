89db98f4717619fb728b6cd7a252328f1f537afc
adehombre <adehombre@gmail.com>
2021-01-15 01:25:08 -0500

    add dynamic GET route

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ app.get('/users', function(req, res){
 	});
 });
 
+app.get('/users/:id', function(req, res){
+    const id = req.params.id;
+	res.json({
+		success: true,
+		message: 'got one user.',
+		user: id
+	});
+});
+
 app.listen(8000, function() {
   console.log("server is running on 8000");
 });
