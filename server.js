7acb32adf0f5a0ca3897dc73bbecd9c116cf7fd0
adehombre <adehombre@gmail.com>
2021-01-15 01:18:09 -0500

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+	{ name: 'Al' },
+    { name: 'John' },
+];
+
+app.get('/users', function(req, res){
+	res.json({
+		success: true,
+		message: 'Successfully got users.',
+		users: mockUserData
+	});
+});
+
 app.listen(8000, function() {
-  console.log("server is running");
-})
+  console.log("server is running on 8000");
+});
